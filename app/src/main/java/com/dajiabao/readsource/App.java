e2707ae399a528195f9b34cec32ad53727f37c28
wangc <274281610@qq.com>
2019-03-21 20:28:35 +0800

    branch1

--- app/src/main/java/com/dajiabao/readsource/App.java
+++ app/src/main/java/com/dajiabao/readsource/App.java
@@ -30,7 +30,7 @@ public class App extends Application {
         }
         LeakCanary.install(this);
 
-        Log.e("wangc","onCreate1");
+        Log.e("wangc","onCreate1 branch");
     }
 
     private void initARouter() {
